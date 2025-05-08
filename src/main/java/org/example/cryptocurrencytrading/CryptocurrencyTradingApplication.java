d0e4fea64c3e9bce3a886696ded30a98b1a3d57d
emkat <emkata.ganev@gmail.com>
2025-05-08 21:22:40 +0300

    Initialize Spring Boot project with Maven Wrapper
    Set up a basic Spring Boot project for cryptocurrency trading. Added Maven wrapper, application properties, and dependencies in `pom.xml`. Included `.gitignore` and test scaffolding to support development workflow.

---
+++ src/main/java/org/example/cryptocurrencytrading/CryptocurrencyTradingApplication.java
@@ -0,0 +1,13 @@
+package org.example.cryptocurrencytrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptocurrencyTradingApplication.class, args);
+    }
+
+}
